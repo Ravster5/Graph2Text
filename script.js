924d72cb6236906a08d867794876214ad1743c99
Ravster5 <72552218+Ravster5@users.noreply.github.com>
2023-10-06 11:35:24 +0100

    css alterations
    Bootstrap removed and everything is now within CSS file.
    Added option to text input or select from drop down for 2 of the elements

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+
+
 function generateSentence() {
   var element1 = document.getElementById("element1").value;
   var element2 = document.getElementById("element2").value;
-  var element3 = document.getElementById("element3").value;
-  var element4 = document.getElementById("element4").value;
+  var element3Select = document.getElementById("element3-select");
+  var element3 = element3Select.value || document.getElementById('element3-input').value;
+  var element4Select = document.getElementById('element4-select');
+  var element4 = element4Select.value || document.getElementById('element4-input').value;
   var element5 = document.getElementById("element5").value;
 
   var sentence = element1 + " " + element2 + " " + element3 + " " + element4 + " " + element5;
